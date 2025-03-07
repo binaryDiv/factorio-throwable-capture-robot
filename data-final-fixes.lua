40b842c4ae7538604c6ea086f4610d5a735e5267
binaryDiv <lexi@binarydiv.dev>
2025-03-07 17:02:58 +0100

    Remove capture bot rocket recycling recipe

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -5,8 +5,9 @@ if utils.settings.get_replace_rocket() then
     -- Replace rocket with capsule in other recipes that use it (Biolab, Captive biter spawner)
     utils.recipes.replace_ingredient_in_all_recipes("capture-robot-rocket", "capture-robot-capsule")
 
-    -- Remove recipe for capture-robot-rocket
+    -- Remove recipe for capture-robot-rocket, as well as its recycling recipe
     data.raw["recipe"]["capture-robot-rocket"] = nil
+    data.raw["recipe"]["capture-robot-rocket-recycling"] = nil
 
     -- Remove capture bot rocket item
     data.raw["ammo"]["capture-robot-rocket"] = nil
